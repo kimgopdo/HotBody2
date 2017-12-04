61e355445b238d5156549deea033c0cbdd6f1831
user1 <user1@user1-PC>
2017-12-04 19:51:59 +0900

    sss

--- src/main/java/com/hotbody/hotShop/payment/PaymentController.java
+++ src/main/java/com/hotbody/hotShop/payment/PaymentController.java
@@ -1,8 +1,13 @@
 package com.hotbody.hotShop.payment;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("hotShop.payment")
 public class PaymentController {
 	
+	@RequestMapping(value="/hotShop/payment")
+	public String paymentForm() {
+		return ".hotShop.payPage";
+	}
 }
