44eadc76c24925430d04155b161fb1b1de17bd0f
user1 <yeri_i@naver.com>
2017-12-21 20:45:34 +0900

    20171221 예리

--- src/main/java/com/hotbody/survey/SurveyController.java
+++ src/main/java/com/hotbody/survey/SurveyController.java
@@ -1,10 +1,12 @@
 package com.hotbody.survey;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,71 +23,147 @@ public class SurveyController {
 	}
 	
 	@RequestMapping(value="/survey/created")
-	public String surveycreated() {
+	public String surveycreated(Model model) {
+		int count = service.surveyCount();
+		List<Survey> list = service.listSurvey();
+		List<Survey> exList = service.readEx();
+				
+		model.addAttribute("surveyList", list);
+		model.addAttribute("count", count+1);
+		model.addAttribute("exList", exList);
 		return ".survey.created";
 	}
 	
 	@RequestMapping(value="/survey/submit")
 	@ResponseBody
-	public Map<String, Object> surveySubmit(@RequestParam Map<String, String> dataMap,
-											@RequestParam int count) {
+	public Map<String, Object> surveySubmit(Survey dto,
+											String[] exContent,
+											int[] exOrder,
+											int[] exScore) {
+		int qSeq = service.qSeq();
+		dto.setQuestionCode(qSeq);
+		service.insertSurvey(dto);
 		
-		Survey dto = new Survey();
-		
-		int qOrder=0;
-		int exOrder=0;
-		
-		for(int a=1;a<=count;a++) {
-			for(Map.Entry<String, String> entry : dataMap.entrySet()) {
-				if(entry.getKey().contains("qOrder")) {
-					System.out.println("오다"+entry.getValue());
-					qOrder = Integer.parseInt(entry.getValue());
-					dto.setqOrder(qOrder);
-				} else if(entry.getKey().contains("qContent"+qOrder+".")) {
-					dto.setqContent(entry.getValue());
-					System.out.println("확인 !!!!!!!!!!!!!!!"+dto.getqContent()+":"+dto.getqOrder());
-				} else if(entry.getKey().contains("sOrO"+qOrder+".")) {
-					dto.setsOrO(Integer.parseInt(entry.getValue()));
-					System.out.println("주?객?"+dto.getsOrO());
-				} else if(entry.getKey().contains("qType"+qOrder+".")) {
-					dto.setqType(entry.getValue());
-					System.out.println("질문유형"+dto.getqType());
-				}
-				
-				//주관식이면 바로 insert
-				if(dto.getqContent() != null && dto.getsOrO()==1) {
-					System.out.println(a+" : insert할꺼임(주ㅅ관식)");
-					dto.setqContent(null);
-					//service.insertSurvey(dto);
-				}
-				
-				//객관식인경우 보기도 가져와야한다.
-				else if(dto.getqContent() != null && dto.getsOrO()==0) {	
-					if (entry.getKey().contains("exOrder"+qOrder+".")) {
-						exOrder = Integer.parseInt(entry.getValue());
-						dto.setExOrder(exOrder);
-						System.out.println("보기번호"+exOrder);
-					} else if (entry.getKey().contains("exContent"+qOrder+"."+exOrder)) {
-						dto.setExContent(entry.getValue());
-						System.out.println("보기내용"+dto.getExContent());
-					} else if (entry.getKey().contains("exScore"+qOrder+"."+exOrder)) {
-						dto.setExScore(Integer.parseInt(entry.getValue()));
-						System.out.println("보기점수"+dto.getExScore());
-					}
-					
-					if(dto.getExContent() !=null && dto.getExScore() != 0) {
-						//service.insertSurvey(dto);
-						System.out.println(a+" : insert할꺼임(객ㅅ관식)");
-						dto.setExContent(null);
-						dto.setExScore(0);
-					}
-					//service.insertSurvey(dto);
-				}
+		if(dto.getSoro()==0) {
+			for(int a=0;a<exOrder.length;a++) {
+				dto.setExContent(exContent[a]);
+				dto.setExOrder(exOrder[a]);
+				dto.setExScore(exScore[a]);
+				service.insertEx(dto);
 			}
 		}
+		
 		Map<String, Object> map = new HashMap<>();
+		List<Survey> list = service.listSurvey();
+		map.put("surveyList", list);
 		map.put("state", "true");
 		return map;
 	}
+	
+	@RequestMapping(value="/survey/updateOrder")
+	@ResponseBody
+	public Map<String, Object> updateOrder(@RequestParam int questionOrder,
+											@RequestParam int type){
+		Map<String, Object> model = new HashMap<>();
+		Map<String, Object> updateMap = new HashMap<>();
+		
+		Survey dto = service.readSurvey(questionOrder);
+		int last = service.surveyCount();
+		
+		if(type==1) {
+			//제일마지막으로
+			if(questionOrder==last) {
+				model.put("state", "last");
+				return model;
+			}
+			for(int a=questionOrder+1;a<=last;a++) {
+				Survey sdto = service.readSurvey(a);
+				updateMap.put("questionCode", sdto.getQuestionCode());
+				updateMap.put("questionOrder", a-1);
+				service.updateOrder(updateMap);
+			}
+			updateMap.put("questionCode", dto.getQuestionCode());
+			updateMap.put("questionOrder", last);
+			service.updateOrder(updateMap);
+		} else if(type==2){
+			//한칸아래로
+			if(questionOrder==last) {
+				model.put("state", "last");
+				return model;
+			}
+			Survey sdto = service.readSurvey(questionOrder+1);
+			updateMap.put("questionCode", sdto.getQuestionCode());
+			updateMap.put("questionOrder", questionOrder);
+			service.updateOrder(updateMap);
+			
+			updateMap.put("questionCode", dto.getQuestionCode());
+			updateMap.put("questionOrder", questionOrder+1);
+			service.updateOrder(updateMap);
+		} else if (type==3) {
+			//한칸위로
+			if(questionOrder==1) {
+				model.put("state", "first");
+				return model;
+			}
+			Survey sdto = service.readSurvey(questionOrder-1);
+			updateMap.put("questionCode", sdto.getQuestionCode());
+			updateMap.put("questionOrder", questionOrder);
+			service.updateOrder(updateMap);
+			
+			updateMap.put("questionCode", dto.getQuestionCode());
+			updateMap.put("questionOrder", questionOrder-1);
+			service.updateOrder(updateMap);
+			
+		} else if (type==4) {
+			//제일 위로
+			if(questionOrder==1) {
+				model.put("state", "first");
+				return model;
+			}
+			for(int a=questionOrder-1;a>=1;a--) {
+				Survey sdto = service.readSurvey(a);
+				updateMap.put("questionCode", sdto.getQuestionCode());
+				updateMap.put("questionOrder", a+1);
+				service.updateOrder(updateMap);
+			}
+			updateMap.put("questionCode", dto.getQuestionCode());
+			updateMap.put("questionOrder", 1);
+			service.updateOrder(updateMap);
+		}
+		
+		model.put("state", "true");
+		return model;
+	}
+	
+	@RequestMapping(value="/survey/delete")
+	@ResponseBody
+	public Map<String, Object> deleteSurvey(@RequestParam int questionOrder){
+		Survey dto = service.readSurvey(questionOrder);
+		int last = service.surveyCount();
+		Map<String, Object> updateMap = new HashMap<>();
+		
+		service.deleteSurvey(dto.getQuestionCode());
+		
+		for(int a=last;a>questionOrder;a--) {
+			Survey sdto = service.readSurvey(a);
+			updateMap.put("questionCode", sdto.getQuestionCode());
+			updateMap.put("questionOrder", a-1);
+			service.updateOrder(updateMap);
+		}
+		
+		Map<String, Object> model = new HashMap<>();
+		
+		model.put("state", "true");
+		return model;
+	}
 
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/hotbody/survey/SurveyServiceImpl.java
+++ src/main/java/com/hotbody/survey/SurveyServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hotbody.survey;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hotbody.common.dao.CommonDAO;
+
 @Service("survey.surveyService")
 public class SurveyServiceImpl implements SurveyService{
 
@@ -11,19 +15,104 @@ public class SurveyServiceImpl implements SurveyService{
 	private CommonDAO dao;
 	
 	@Override
+	public int qSeq() {
+		int qSeq = 0;
+		try {
+			qSeq = dao.selectOne("survey.qSeq");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return qSeq;
+	}
+	
+	@Override
 	public int insertSurvey(Survey dto) {
+		try {
+			dao.insertData("survey.insertSurvey", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dto.getQuestionCode();
+	}
+
+	@Override
+	public int surveyCount() {
+		int count = 0;
+		try {
+			count = dao.selectOne("survey.surveyCount");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	@Override
+	public int insertEx(Survey dto) {
 		int result = 0;
 		try {
-			int qSeq = dao.selectOne("survey.qSeq");
-			dto.setqCode(qSeq);
-
-			result = dao.insertData("survey.insertSurvey", dto);
-			if(dto.getsOrO()==0) {
-				dao.insertData("survey.insertEx", dto);
-			}
+			result = dao.insertData("survey.insertEx", dto);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
+
+	@Override
+	public List<Survey> listSurvey() {
+		List<Survey> list = null;
+		try {
+			list = dao.selectList("survey.listSurvey");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public List<Survey> readEx() {
+		List<Survey> list = null;
+		try {
+			list = dao.selectList("survey.readEx");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public Survey readSurvey(int qorder) {
+		Survey dto = null;
+		try {
+			dto = dao.selectOne("survey.readSurvey", qorder);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dto;
+	}
+
+	@Override
+	public int updateOrder(Map<String, Object> map) {
+		int result = 0;
+		try {
+			result = dao.updateData("survey.updateOrder", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public int deleteSurvey(int questionCode) {
+		int result = 0;
+		try {
+			result = dao.deleteData("survey.deleteEx", questionCode);
+			dao.deleteData("survey.deleteSurveyResult", questionCode);
+			dao.deleteData("survey.deleteSurvey", questionCode);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	
 }
